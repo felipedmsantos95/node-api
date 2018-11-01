771c473a0ccde96d87085076ffecbf68264e6c39
Felipe Santos <fdms@icomp.ufam.edu.br>
2018-11-01 15:15:34 -0400

    firt commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+
+//Inicio do app
+const app = express();
+app.use(express.json());
+
+//Inicio DB
+
+mongoose.connect(
+	'mongodb://localhost:27017/nodeapi', 
+	{useNewUrlParser: true}
+);
+requireDir("./src/models");
+
+//Rotas
+app.use('/', require('./src/routes'));
+
+app.listen(3000);
---
+++ src/controllers/ProductController.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const Product = mongoose.model('Product');
+
+module.exports = {
+	async index(req, res){//Busca produtos cadastrados
+
+		const { page = 1 } = req.query;
+		const products = await Product.paginate({}, { page, limit: 5});
+
+		return res.json(products);
+
+	},
+
+	async show(req, res) {
+		const product = await Product.findById(req.params.id);
+
+		return res.json(product);
+	},
+
+	async store(req, res){//Criação de produtos
+		const product = await Product.create(req.body);
+
+		return res.json(product);
+
+	},
+
+	async update(req, res){
+		const product = await Product.findByIdAndUpdate(req.params.id, req.body, { 
+			new: true
+		});
+
+		return res.json(product);
+	},
+
+	async destroy(req, res){
+		await Product.findByIdAndRemove(req.params.id);
+
+		return res.send();
+	}
+};
